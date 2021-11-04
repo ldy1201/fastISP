bc9bf2164acf68f4c1569cc317a31bd32c2d18fa
lidunyu <chaodaliyu1021@sina.com>
2021-11-04 21:46:43 +0800

    updata utils file

--- include/ISPDefine.h
+++ include/ISPDefine.h
@@ -30,14 +30,6 @@
         }                                                        \
     }
 
-#define ISP_ASSERT(x)                                            \
-    {                                                            \
-        int res = (x);                                           \
-        if (!res) {                                              \
-            ISP_ERROR("Error for %s, %d\n", __FILE__, __LINE__); \
-        }                                                        \
-    }
-
 #define FUNC_PRINT(x) ISP_PRINT(#x "=%d in %s, %d \n", x, __func__, __LINE__);
 #define FUNC_PRINT_ALL(x, type) ISP_PRINT(#x "=" #type " %" #type " in %s, %d \n", x, __func__, __LINE__);
 
